ca913d4d5498b30bade4931f43c9df02a03df90e
danbran <daniel.brandt@tu-berlin.de>
2017-12-28 12:45:19 +0100

    init pelican

---
+++ pelicanconf.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'danbran'
+SITENAME = 'shiny-python'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+STATIC_PATHS = ['blog','python']
+
+ARTICLE_URL = '{category}/{slug}.html'
+ARTICLE_SAVE_AS = '{category}/{slug}.html'
+
+FEED_MAX_ITEMS = 10
+
+#EXTRA_PATH_METADATA = {
+#    'extra/favicon.ico': {'path': 'favicon.ico'},
+#    'extra/CNAME': {'path': 'CNAME'},
+#}
+
+ARTICLE_EXCLUDES = ['in_progress']
+
+PLUGIN_PATHS = ['plugins']
+# PLUGINS = ['render_math', 'tipue_search' , 'sitemap']
+
+DIRECT_TEMPLATES = ['index']#, 'search']
+
+CATEGORY_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+TAG_SAVE_AS = ''
+AUTHORS_SAVE_AS = ''
+CATEGORIES_SAVE_AS = ''
+TAGS_SAVE_AS = ''
+ARCHIVES_SAVE_AS = ''
+
+DELETE_OUTPUT_DIRECTORY = True
+
+# SITEMAP = {
+#     'format': 'xml',
+#     'priorities': {
+#        'articles': 0.5,
+#        'indexes': 0.5,
+#        'pages': 0.5
+#    },
+#    'changefreqs': {
+#        'articles': 'monthly',
+#        'indexes': 'daily',
+#        'pages': 'monthly'
+#    }
+#}
+#
+#MD_EXTENSIONS = ['codehilite(guess_lang=False)', 'extra']
